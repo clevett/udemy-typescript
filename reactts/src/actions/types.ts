6f3639f2d02d62c4a7bcf043a5ced3b7b9396585
Cassie <cassielevett@gmail.com>
2020-12-12 11:20:35 -0600

    save lesson 17

--- reactts/src/actions/types.ts
+++ reactts/src/actions/types.ts
@@ -1,4 +1,8 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos'
+
 export enum ActionsTypes {
-  fetchTodos //will default to 0 followed by 1, 2....
+  fetchTodos, //will default to 0 followed by 1, 2....
+  deleteTodo
 }
 
+export type Action = FetchTodosAction | DeleteTodoAction
